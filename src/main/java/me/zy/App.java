21d66d387eeed8e61310dc751defe3305dc4e0e7
zhaoying <iszhao@foxmail.com>
2019-09-22 03:20:20 +0800

    通过注解方式(引入Spring框架)实现远程调用

--- src/main/java/me/zy/App.java
+++ src/main/java/me/zy/App.java
@@ -1,13 +1,21 @@
 package me.zy;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 /**
  * Hello world!
  *
  */
 public class App {
     public static void main( String[] args ){
-        RpcProxyClient rpcProxyClient = new RpcProxyClient();
+       /* RpcProxyClient rpcProxyClient = new RpcProxyClient();
         IHelloService helloService = rpcProxyClient.clientProxy(IHelloService.class,"localhost",8080);
-        helloService.sayHello("haha");
+        helloService.sayHello("haha");*/
+
+        ApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
+        RpcProxyClient rpcProxyClient1 = context.getBean(RpcProxyClient.class);
+        IHelloService helloService = rpcProxyClient1.clientProxy(IHelloService.class,"localhost",8080);
+        helloService.sayHello("v1.1 by spring.");
     }
 }
